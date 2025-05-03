ba42a08c549452a0e69df660cb938d3182969b89
Margaux <margaux.tarayre@gmail.com>
2025-05-03 14:39:20 +0200

    Creation of rating/ file to separate style

---
+++ ituneApp/components/rating/StarRating.styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  starButton: {
+    padding: 3,
+    marginRight: 2,
+  },
+  ratingText: {
+    marginLeft: 5,
+    fontSize: 12,
+    color: '#666',
+  },
+});
+
+export default styles;
